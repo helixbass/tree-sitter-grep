46abf52abe7dd1e4be20257186604ed02572f77f
Julian Rosse <julian@helixbass.net>
2023-07-18 08:45:25 -0400

    windows tests

--- tests/shared.rs
+++ tests/shared.rs
@@ -146,7 +146,7 @@ fn strip_trailing_carriage_return(line: &str) -> Cow<'_, str> {
 }
 
 fn normalize_match_path(line: &str) -> Cow<'_, str> {
-    regex!(r#"^(?:\u{1b}[\d+m)*[a-zA-Z_\-\\/]+\.(?:rs|js|tsx)"#)
+    regex!(r#"^(?:\u{1b}\[\d+m)*[a-zA-Z_\-\\/]+\.(?:rs|js|tsx)"#)
         .replace(line, |captures: &Captures| captures[0].replace('\\', "/"))
 }
 
